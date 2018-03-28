470e14379b0da94c2b0624c80a9874a578e463cc
ajay.singh721@gmail.com <ajay.singh721@gmail.com>
2018-03-29 01:25:42 +0530

    Project structure for MedoHelp

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+const express = require('express');
+
+// API files to interact with MongoDB
+const api = require('./server/routes/api');
+const app = express();
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Angular CLIENT output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// API location
+app.use('/api', api);
+
+// Send all other request to Angular app
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/index.html'));
+})
+
+// Set port
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Running on localhost:${port}`);
+})
+
+
+
+
+
---
+++ server/routes/api.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017/dev', (err, db) => {
+        if (err) {
+            console.log(err)
+            db.close();
+            return;
+        }
+        else
+            closure(db);
+    })
+}
+
+// Response object
+let response = {
+    status: 200,
+    data: [],
+    message: null
+}
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+}
+
+// Get employees
+router.get('/employees', (req, res) => {
+    connection((db) => {
+        db.collection('persons')
+            .findOne()
+            .toArray()
+            .then((employees) => {
+                response.data = employees;
+                res.join(response);
+            })
+            .catch((err) => {
+                sendError(err);
+            });
+    })
+});
+
+module.exports = router;
+
